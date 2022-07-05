9763469d5fcfb68ea607f343d7588e2267b5f305
Jeremy Pinto <jerpint@gmail.com>
2022-07-05 14:41:06 -0400

    Add simple model, strip train loop to bare minimum

--- bigearthnet/models/my_model.py
+++ bigearthnet/models/my_model.py
@@ -2,54 +2,39 @@ import logging
 import typing
 import torch
 import pytorch_lightning as pl
-
-from bigearthnet.models.optim import load_loss, load_optimizer
-
-from bigearthnet.utils.hp_utils import check_and_log_hp
+import torch.nn as nn
 
 logger = logging.getLogger(__name__)
 
 
-class BaseModel(pl.LightningModule):
+class LitModel(pl.LightningModule):
     """Base class for Pytorch Lightning model - useful to reuse the same *_step methods."""
+    def __init__(self):
+        super().__init__()
+        self.loss_fn = torch.nn.BCEWithLogitsLoss()
 
     def configure_optimizers(self):
-        """Returns the combination of optimizer(s) and learning rate scheduler(s) to train with.
+        optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
+        return optimizer
 
-        Here, we read all the optimization-related hyperparameters from the config dictionary and
-        create the required optimizer/scheduler combo.
-
-        This function will be called automatically by the pytorch lightning trainer implementation.
-        See https://pytorch-lightning.readthedocs.io/en/latest/common/optimizers.html for more info
-        on the expected returned elements.
-        """
-        # we use the generic loading function from the `model_loader` module, but it could be made
-        # a direct part of the model (useful if we want layer-dynamic optimization)
-        return load_optimizer(self.hparams, self)
-
-    def _generic_step(
-            self,
-            batch: typing.Any,
-            batch_idx: int,
-    ) -> typing.Any:
+    def _generic_step(self, batch, batch_idx):
         """Runs the prediction + evaluation step for training/validation/testing."""
-        input_data, targets = batch
-        preds = self(input_data)  # calls the forward pass of the model
-        loss = self.loss_fn(preds, targets)
+        inputs = batch['data']
+        targets = batch['labels']
+        outputs = self(inputs)
+        loss = self.loss_fn(outputs, targets.float())
         return loss
 
     def training_step(self, batch, batch_idx):
         """Runs a prediction step for training, returning the loss."""
         loss = self._generic_step(batch, batch_idx)
-        self.log("train_loss", loss)
-        self.log("epoch", self.current_epoch)
-        self.log("step", self.global_step)
-        return loss  # this function is required, as the loss returned here is used for backprop
+        self.log("train_loss", loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
+        return loss
 
     def validation_step(self, batch, batch_idx):
         """Runs a prediction step for validation, logging the loss."""
         loss = self._generic_step(batch, batch_idx)
-        self.log("val_loss", loss)
+        self.log("val_loss", loss, on_step=False, on_epoch=True, prog_bar=True, logger=True)
 
     def test_step(self, batch, batch_idx):
         """Runs a prediction step for testing, logging the loss."""
@@ -57,36 +42,45 @@ class BaseModel(pl.LightningModule):
         self.log("test_loss", loss)
 
 
-class MyModel(BaseModel):  # pragma: no cover
+class SimpleModel(LitModel):  # pragma: no cover
     """Simple Model Class.
 
     Inherits from the given framework's model class. This is a simple MLP model.
     """
 
-    def __init__(self, hyper_params: typing.Dict[typing.AnyStr, typing.Any]):
+    def __init__(
+        self,
+        num_classes,
+    ):
         """__init__.
 
         Args:
             hyper_params (dict): hyper parameters from the config file.
         """
-        super(MyModel, self).__init__()
+        super(SimpleModel, self).__init__()
+        self.conv_layers = nn.Sequential(
+            nn.Conv2d(3, 64, 5),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+            nn.Conv2d(64, 64, 5),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+            nn.Conv2d(64, 64, 5),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+            nn.Conv2d(64, 64, 5),
+            nn.Conv2d(64, 64, 5),
+            nn.ReLU(),
+        )
+        self.flatten = nn.Flatten()
+        self.mlp_layers = nn.Sequential(
+            nn.Linear(576, 128),  # The input size for the linear layer is determined by the previous operations
+            nn.ReLU(),
+            nn.Linear(128, num_classes),  # Here we get exactly num_classes logits at the output
+        )
 
-        check_and_log_hp(['size'], hyper_params)
-        self.save_hyperparameters(hyper_params)  # they will become available via model.hparams
-        self.linear1 = torch.nn.Linear(5, hyper_params['size'])
-        self.linear2 = torch.nn.Linear(hyper_params['size'], 1)
-        self.loss_fn = load_loss(hyper_params)  # 'load_loss' could be part of the model itself...
-
-    def forward(self, data):
-        """Forward method of the model.
-
-        Args:
-            data (tensor): The data to be passed to the model.
-
-        Returns:
-            tensor: the output of the model computation.
-
-        """
-        hidden = torch.nn.functional.relu(self.linear1(data))
-        result = self.linear2(hidden)
-        return result.squeeze()
+    def forward(self, x):
+        x = self.conv_layers(x)
+        x = self.flatten(x)  # Flatten is necessary to pass from CNNs to MLP
+        x = self.mlp_layers(x)
+        return x
