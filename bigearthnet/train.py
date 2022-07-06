6f9d2e1d318bbe5482443965a1fc6aa84aa66e40
Jeremy Pinto <jerpint@gmail.com>
2022-07-06 16:53:27 -0400

    Add hydra support

--- bigearthnet/train.py
+++ bigearthnet/train.py
@@ -1,6 +1,7 @@
-import pathlib
 import pytorch_lightning as pl
 from pytorch_lightning.callbacks import ModelCheckpoint
+import hydra
+from omegaconf import DictConfig
 
 from bigearthnet.data.data_loader import load_datamodule
 from bigearthnet.models.model_loader import load_model
@@ -26,13 +27,18 @@ def load_callbacks():
 
     return callbacks
 
-# initialize the model + datamodules
-hub_dataset_path = pathlib.Path("./data/debug_dataset/hub_dataset/")
-datamodule = load_datamodule(hub_path=hub_dataset_path, batch_size=16)
-model = load_model(architecture="SimpleModel", num_classes=43)
-callbacks = load_callbacks()
 
-# do the training
-trainer = pl.Trainer(max_epochs=5)  # gpus=4, num_nodes=8, precision=16, limit_train_batches=0.5)
-trainer = pl.Trainer(max_epochs=5, callbacks=callbacks)
-trainer.fit(model, datamodule=datamodule)
+@hydra.main(config_path="configs", config_name="config")
+def main(cfg: DictConfig):
+
+    # data setup
+    datamodule = load_datamodule(**cfg.datamodule)
+    model = load_model(**cfg.model)
+    callbacks = load_callbacks()
+
+    # do the training
+    trainer = pl.Trainer(**cfg.trainer, callbacks=callbacks)
+    trainer.fit(model, datamodule=datamodule)
+
+if __name__ == "__main__":
+    main()
