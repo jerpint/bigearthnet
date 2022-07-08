341534e0ed19313f9591ea0b069ddd0adaf06581
Jeremy Pinto <jerpint@gmail.com>
2022-07-08 10:07:35 -0400

    update path

--- bigearthnet/data/scripts/prepare_dataset.sh
+++ bigearthnet/data/scripts/prepare_dataset.sh
@@ -1,4 +1,4 @@
 python ../data_parser.py \
---root-path "$SCRATCH/bigearth/BigEarthNet-v1.0/" \
---splits-path "$SCRATCH/bigearth/splits/" \
---output-hub-path "$SCRATCH/bigearth/bigearthnet-hub/" \
+--root-path "$SCRATCH/bigearthnet/BigEarthNet-v1.0/" \
+--splits-path "$SCRATCH/bigearthnet/splits/" \
+--output-hub-path "$SCRATCH/bigearthnet/bigearthnet-hub/" \
--- bigearthnet/data/scripts/prepare_mini_dataset.sh
+++ bigearthnet/data/scripts/prepare_mini_dataset.sh
@@ -1,13 +1,13 @@
 python ../prepare_mini_dataset.py \
---splits-dir "$SCRATCH/bigearth/splits/" \
---output-dir "$SCRATCH/bigearth/bigearthnet-mini/" \
---dataset-root-dir $SCRATCH/bigearth/BigEarthNet-v1.0 \
+--splits-dir "$SCRATCH/bigearthnet/splits/" \
+--output-dir "$SCRATCH/bigearthnet/bigearthnet-mini/" \
+--dataset-root-dir $SCRATCH/bigearthnet/BigEarthNet-v1.0 \
 --split-samples 80 10 10 \
 
 python ../data_parser.py \
---root-path "$SCRATCH/bigearth/bigearthnet-mini/data/" \
---splits-path "$SCRATCH/bigearth/bigearthnet-mini/splits/" \
---output-hub-path "$SCRATCH/bigearth/bigearthnet-mini/" \
+--root-path "$SCRATCH/bigearthnet/bigearthnet-mini/data/" \
+--splits-path "$SCRATCH/bigearthnet/bigearthnet-mini/splits/" \
+--output-hub-path "$SCRATCH/bigearthnet/bigearthnet-mini/" \
 
-cd $SCRATCH/bigearth/
+cd $SCRATCH/bigearthnet/
 tar  -zcf bigearthnet-mini.tar bigearthnet-mini/
