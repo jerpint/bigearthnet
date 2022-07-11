694bfcd2579eb401aec3db1cc6aa48e679cbf6b9
Jeremy Pinto <jerpint@gmail.com>
2022-07-11 10:25:36 -0400

    fix paths for medium dataset

--- bigearthnet/data/scripts/prepare_medium_dataset.sh
+++ bigearthnet/data/scripts/prepare_medium_dataset.sh
@@ -2,15 +2,15 @@ export BUGGER_OFF=True
 
 python ../prepare_mini_dataset.py \
 --splits-dir "$SCRATCH/bigearthnet/splits/" \
---output-dir "$SCRATCH/bigearthnet/bigearthnet-mini/" \
+--output-dir "$SCRATCH/bigearthnet/bigearthnet-medium/" \
 --dataset-root-dir $SCRATCH/bigearthnet/BigEarthNet-v1.0 \
 --seed 42 \
 --split-samples 4000 500 500 \
 
 python ../data_parser.py \
---root-path "$SCRATCH/bigearthnet/bigearthnet-mini/data/" \
---splits-path "$SCRATCH/bigearthnet/bigearthnet-mini/splits/" \
---output-hub-path "$SCRATCH/bigearthnet/bigearthnet-mini/" \
+--root-path "$SCRATCH/bigearthnet/bigearthnet-medium/data/" \
+--splits-path "$SCRATCH/bigearthnet/bigearthnet-medium/splits/" \
+--output-hub-path "$SCRATCH/bigearthnet/bigearthnet-medium/" \
 
 cd $SCRATCH/bigearthnet/
 tar  -zcf bigearthnet-mini.tar bigearthnet-mini/
