1ff7217e61cf68b065f2147fcfb23ac14f9faf85
Jeremy Pinto <jerpint@gmail.com>
2022-07-11 10:27:04 -0400

    remove extra space

--- bigearthnet/data/scripts/prepare_mini_dataset.sh
+++ bigearthnet/data/scripts/prepare_mini_dataset.sh
@@ -13,4 +13,4 @@ python ../data_parser.py \
 --output-hub-path "$SCRATCH/bigearthnet/bigearthnet-mini/" \
 
 cd $SCRATCH/bigearthnet/
-tar  -zcf bigearthnet-mini.tar bigearthnet-mini/
+tar -zcf bigearthnet-mini.tar bigearthnet-mini/
