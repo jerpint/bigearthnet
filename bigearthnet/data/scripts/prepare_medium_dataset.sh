647eaddc8d62a4fa5ed137eb3697afa2402f31db
Jeremy Pinto <jerpint@gmail.com>
2022-07-11 12:55:51 -0400

    Add medium dataset generation script (#7)
    * Add script for medium dataset
    * Disable bug tracking from hub

---
+++ bigearthnet/data/scripts/prepare_medium_dataset.sh
@@ -0,0 +1,18 @@
+export BUGGER_OFF=True
+
+python ../prepare_mini_dataset.py \
+--splits-dir "$SCRATCH/bigearthnet/splits/" \
+--output-dir "$SCRATCH/bigearthnet/bigearthnet-medium/" \
+--dataset-root-dir $SCRATCH/bigearthnet/BigEarthNet-v1.0 \
+--seed 42 \
+--split-samples 4000 500 500 \
+
+python ../data_parser.py \
+--root-path "$SCRATCH/bigearthnet/bigearthnet-medium/data/" \
+--splits-path "$SCRATCH/bigearthnet/bigearthnet-medium/splits/" \
+--output-hub-path "$SCRATCH/bigearthnet/bigearthnet-medium/" \
+
+echo "compressing folder to tar archive..."
+cd $SCRATCH/bigearthnet/
+tar  -zcf bigearthnet-medium.tar bigearthnet-medium/
+echo "done"
--- bigearthnet/data/scripts/prepare_mini_dataset.sh
+++ bigearthnet/data/scripts/prepare_mini_dataset.sh
@@ -1,13 +1,18 @@
+export BUGGER_OFF=True
+
 python ../prepare_mini_dataset.py \
 --splits-dir "$SCRATCH/bigearthnet/splits/" \
 --output-dir "$SCRATCH/bigearthnet/bigearthnet-mini/" \
 --dataset-root-dir $SCRATCH/bigearthnet/BigEarthNet-v1.0 \
 --split-samples 80 10 10 \
+--seed 42 \
 
 python ../data_parser.py \
 --root-path "$SCRATCH/bigearthnet/bigearthnet-mini/data/" \
 --splits-path "$SCRATCH/bigearthnet/bigearthnet-mini/splits/" \
 --output-hub-path "$SCRATCH/bigearthnet/bigearthnet-mini/" \
 
+echo "compressing folder to tar archive..."
 cd $SCRATCH/bigearthnet/
-tar  -zcf bigearthnet-mini.tar bigearthnet-mini/
+tar -zcf bigearthnet-mini.tar bigearthnet-mini/
+echo "done"
